c3b369ff0f36301c221d18e8bdc6e9b3fddfba6d
Joycelyn Chen <jwchen.tw@gmail.com>
2023-12-22 22:51:38 -0800

    slicing program modified

--- Data/hdf5topng.py
+++ Data/hdf5topng.py
@@ -5,14 +5,16 @@ import os
 import glob
 from astropy import units as u
 
-root_folder = "/Users/joycelynchen/Desktop/UBC/Thesis/Data/synthetic_data/synthetic_data"
-data_dir = "/Users/joycelynchen/Desktop/UBC/Thesis/Data/synthetic_data/augmented"
+root_folder = "/Users/joycelynchen/Desktop/UBC/Research/Data/synthetic_data/sn34"
+data_dir = "/Users/joycelynchen/Desktop/UBC/Research/Data/synthetic_data/sn34/img"
 file_prefix = "sn34_smd132_bx5_pe300_hdf5_plt_cnt_"
 
 # 0200 ~ 1900
-lower_limit = 190
-upper_limit = 300
+lower_limit = 201
+upper_limit = 202
 offset = 1
+total_slice = 800
+
 for i in range(lower_limit, upper_limit + 1, offset):
     if (i < 1000):
         filename = f"{file_prefix}0{i}"
@@ -23,14 +25,15 @@ for i in range(lower_limit, upper_limit + 1, offset):
     ds = yt.load(os.path.join(root_folder, filename))
     #ds.current_time, ds.current_time.to('Myr')
 
+
     # Saving img
-    for j in range(0, 100, 10):
+    for j in range(int(-(total_slice / 2)), int(total_slice / 2), 1):
         f = yt.SlicePlot(ds, 'z', 'dens', center = [0, 0, j]*yt.units.pc)
         f.hide_axes()
         f.hide_colorbar()
 #         f.show()
-        f.save(os.path.join(data_dir, f'{filename}_z{j}.jpg'))
+        f.save(os.path.join(data_dir, f'{filename}_z{j + (total_slice / 2)}.jpg'))
+        # print(os.path.join(data_dir, f'{filename}_z{j + (total_slice / 2)}.jpg'))
 #     f.hide_axes()
 #     f.hide_colorbar()
 #     f.save(os.path.join(data_dir, f'{filename}.jpg')) # can be png, pdf, etc
-
