dc992e974656e11d8cfb9fdff62a47f349b1b9f5
Joycelyn Chen <jwchen.tw@gmail.com>
2023-12-30 23:47:53 -0800

    fixed hdf5topng float number naming issue

--- Data/hdf5topng.py
+++ Data/hdf5topng.py
@@ -5,13 +5,13 @@ import os
 # import glob
 # from astropy import units as u
 
-root_folder = "/Users/joycelynchen/Desktop/UBC/Research/Data/synthetic_data/sn34"
-data_dir = "/Users/joycelynchen/Desktop/UBC/Research/Data/synthetic_data/sn34/img"
+root_folder = "/Users/joycelynchen/Desktop/UBC/Research/Program/Dataset/210_219"
+data_dir = "/Users/joycelynchen/Desktop/UBC/Research/Program/Dataset/210_219"
 file_prefix = "sn34_smd132_bx5_pe300_hdf5_plt_cnt_"
 
 # 0200 ~ 1900
-lower_limit = 201
-upper_limit = 202
+lower_limit = 210
+upper_limit = 219
 offset = 1
 total_slice = 800
 
@@ -25,12 +25,11 @@ for i in range(lower_limit, upper_limit + 1, offset):
     ds = yt.load(os.path.join(root_folder, filename))
     #ds.current_time, ds.current_time.to('Myr')
 
-
     # Saving img
     for j in range(int(-(total_slice / 2)), int(total_slice / 2), 1):
         f = yt.SlicePlot(ds, 'z', 'dens', center = [0, 0, j]*yt.units.pc)
         f.hide_axes()
         f.hide_colorbar()
-#         f.show()
-        f.save(os.path.join(data_dir, f'{filename}_z{j + (total_slice / 2)}.jpg'))
+#       f.show()
+        f.save(os.path.join(data_dir, str(i), f'{filename}_z{int(j + (total_slice / 2))}.jpg'))
         
