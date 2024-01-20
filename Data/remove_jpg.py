54627689d88b117d144987e538d216369767df77
Joycelyn-Chen <jwchen.tw@gmail.com>
2024-01-20 14:22:37 -0800

    added the program to remove the unwanted jpg files within the dataset, so that each mask correspond to one raw image

---
+++ Data/remove_jpg.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+
+def remove_unmatched_jpg(folder_path):
+    jpg_files = [f for f in os.listdir(folder_path) if f.endswith('.jpg')]
+    
+    for jpg_file in jpg_files:
+        jpg_path = os.path.join(folder_path, jpg_file)
+        png_path = os.path.join(folder_path, jpg_file.replace('.jpg', '.png'))
+
+        if not os.path.exists(png_path):
+            print(f"Removing {jpg_path} (no corresponding .png file)")
+            os.remove(jpg_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_folder", help="The path to the dataset") 
+    args = parser.parse_args()
+
+    remove_unmatched_jpg(args.path_to_folder)
