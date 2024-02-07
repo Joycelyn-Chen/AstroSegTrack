34027618675a45dd0409c4ab65996c5f1698119e
Joycelyn-Chen <jwchen.tw@gmail.com>
2024-02-06 18:51:39 -0800

    added debug

--- analysis/track_volume.py
+++ analysis/track_volume.py
@@ -16,7 +16,12 @@ low_x0, low_y0, low_w, low_h, bottom_z, top_z = -500, -500, 1000, 1000, -500, 50
 
 def filter_data(df, time_range, posz_pc_range):
     # Filter the DataFrame based on specified conditions
-    return df[(df['time_Myr'] >= time_range[0] & df['time_Myr'] < time_range[1]) & (df['posz_pc'].between(posz_pc_range[0], posz_pc_range[1]))]
+    if posz_pc_range[0] < posz_pc_range[1]:
+        lower_z, upper_z = posz_pc_range[0], posz_pc_range[1]
+    else:
+        lower_z, upper_z = posz_pc_range[1], posz_pc_range[0]
+
+    return df[(df['time_Myr'] >= time_range[0] & df['time_Myr'] < time_range[1]) & (df['posz_pc'] >= lower_z & df['posz_pc'] <= upper_z)]
 
 
 def associate_slices_within_cube(start_z, end_z, image_paths, mask, mask_dir_root, timestamp, direction):
