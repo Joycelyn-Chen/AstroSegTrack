34b367f70ba34fd7fcc790fef3b1e755c190f0e0
Jing-Wen(Joycelyn) Chen <joy0921@elephant.ws.ok.ubc.ca>
2024-03-06 14:48:29 -0800

    Alex made some changes in Los_velosity_bins.py

--- analysis/LoS_velosity_bins.py
+++ analysis/LoS_velosity_bins.py
@@ -4,8 +4,8 @@ import yt
 import numpy as np
 import os 
 
-# hdf5_root = "/srv/data/stratbox_simulations/stratbox_particle_runs/bx5/smd132/sn34/pe300/4pc_resume/4pc"
-hdf5_root = "/home/joy0921/Desktop/Dataset/hdf5_files"
+hdf5_root = "/srv/data/stratbox_simulations/stratbox_particle_runs/bx5/smd132/sn34/pe300/4pc_resume/4pc"
+# hdf5_root = "/home/joy0921/Desktop/Dataset/hdf5_files"
 
 ds = yt.load(os.path.join(hdf5_root, 'sn34_smd132_bx5_pe300_hdf5_plt_cnt_0206'))
 
@@ -24,20 +24,21 @@ def get_velx_dens(x, y, z):
     posz_pc = int(z*256/1000)
 
     velz = obj["flash", "velz"][posx_pc, posy_pc, -250:250].to('km/s').value        # -250:250
-    dens = obj["flash", "dens"][posx_pc, posy_pc, -250:250].value                   # -250:250
+    dens = obj["flash", "dens"][posx_pc, posy_pc, -250:250].to('g/cm**3').value                   # -250:250
 
-    dz = (3.9 * 3.1 * 10**18) / (1.7 * 10**-24)
-    dens = dens * dz
+    dz = obj['flash', 'dz'][posx_pc, posy_pc, -250:250].to('cm').value
+    mp = yt.physical_constants.mp.value # proton mass
+    coldens = dens * dz / (1.4 * mp)
 
     # fig, ax = plt.subplots()
     # ax.plot(velz)
     # axr = ax.twinx()
     # axr.plot(dens, 'k')
-    return velz, dens
+    return velz, coldens
 
 velz = []
 dens = []
-coords = [(-333, -443, -91)] #, (-323, -443, -91), (-333, -453, -91), (-323, -453, -91)]
+coords = [(-333, -443, -91), (-323, -443, -91), (-333, -453, -91), (-323, -453, -91)]
 
 for coord in coords:
     new_velz, new_dens = get_velx_dens(coord[0], coord[1], coord[2])
@@ -77,12 +78,32 @@ plt.savefig("LoS.png")
 plt.clf()
 
 fig, ax = plt.subplots()
-ax.plot(velz)
+ax.plot(velz, label='$v_z$')
 axr = ax.twinx()
 axr.plot(dens, 'k')
 ax.set_xlabel('# data points')
 ax.set_ylabel('Velocity (km/s)')
-axr.set_ylabel('Density (cm^-2)')
+axr.set_ylabel('Column Density (cm^-2)')
 ax.set_title('Line of Sight velosity')
+ax.legend()
 
 plt.savefig("LoS_dens.png")
+
+fig, ax = plt.subplots()
+ax.plot(velz, dens, 'k-')
+ax.set_xlabel('$v_z$')
+ax.set_ylabel('column density (cm$^{-2}$)')
+ax.set_xlim(1, 9)
+
+fig.savefig('dens_velz.png')
+
+idx = np.argmin( np.abs(velz-2.5))
+print(idx)
+print(dens.shape)
+
+fig, ax = plt.subplots()
+im = ax.imshow(np.log10(dens[idx]))
+
+fig.colorbar(im)
+
+fig.savefig('channel_image.png')
