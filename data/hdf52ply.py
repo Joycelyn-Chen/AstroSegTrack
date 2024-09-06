e1f0ab418500df5c9c194d5879a47dba3175484c
Joycelyn Chen <joy0921@elephant.ws.ok.ubc.ca>
2024-09-05 18:11:51 -0700

    editted the hdf52ply.py to use temperature as thresholding method as well

--- data/hdf52ply.py
+++ data/hdf52ply.py
@@ -7,14 +7,20 @@ import argparse
 import cv2
 import open3d as o3d
 
-DEBUG = True
+DEBUG = False
 
 
-def apply_otsus_thresholding(image):
+def apply_otsus_thresholding(image, threshold = 120, height = 100):
     # _, threshold = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     
     # return threshold
-    _, binary_image = cv2.threshold(image.astype("uint8"), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    THESHOLD, binary_image = cv2.threshold(image.astype("uint8"), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # _, binary_image = cv2.threshold(image.astype("uint8"), 0, 255, round(THESHOLD * (height + 1 / 100)))  
+    
+    if (DEBUG):
+        print(f"temperature threshold: {THESHOLD}")  
+
     return cv2.bitwise_not(binary_image)
 
 def within_range(min, max, target):
@@ -41,7 +47,7 @@ def plot_dens_z(dens, center_z):
     # fig.savefig(f'../expanding_velocity/{time_Myr}/dzoom_{center_z}.png')
     plt.show()
 
-def get_velz_dens(obj, x_range, y_range, z_range):
+def get_velz_dens_temp(obj, x_range, y_range, z_range):
     # read a 3D grid of velz and density array
     velz = obj["flash", "velz"][x_range[0] : x_range[1], y_range[0] : y_range[1], z_range[0] : z_range[1]].to('km/s').value        
     dens = obj["flash", "dens"][x_range[0] : x_range[1], y_range[0] : y_range[1], z_range[0] : z_range[1]].to('g/cm**3').value        
@@ -111,33 +117,34 @@ def main(args):
         y_range_scaled = (0, args.ylim - 1)  
         z_range_scaled = (0, args.zlim - 1)
 
-        _, new_dens, _ = get_velz_dens(obj, x_range_scaled, y_range_scaled, z_range_scaled)
+        _, density, temperature = get_velz_dens_temp(obj, x_range_scaled, y_range_scaled, z_range_scaled) 
 
 
 
         # input_point = (center_x, center_y)
         mask = np.zeros((args.pixel_boundary, args.pixel_boundary, args.upper_bound - args.lower_bound))
 
-        for current_z in range(args.upper_bound - args.lower_bound):
-            dens_slice = normalize4thresholding(new_dens[:, :, current_z + args.lower_bound]) 
+        # for current_z in range(args.upper_bound - args.lower_bound):
+        #     temp_slice = normalize4thresholding(temperature[:, :, current_z + args.lower_bound]) 
             
-            # threshold + connected component
-            binary_mask = apply_otsus_thresholding(dens_slice)
-            _, labels, _, _ = cv2.connectedComponentsWithStats(binary_mask, connectivity=8)
+        #     # threshold + connected component
+        #     binary_mask = apply_otsus_thresholding(temp_slice, args.threshold, height=current_z)
+        #     _, labels, _, _ = cv2.connectedComponentsWithStats(binary_mask, connectivity=8)
 
-            # retrieve all mask only
-            i = 0
-            binary_mask = labels == i
-            binary_mask = ~binary_mask
-            mask[:, :, current_z] = binary_mask
+        #     # retrieve all mask only
+        #     i = 0
+        #     binary_mask = labels == i
+        #     binary_mask = ~binary_mask
+        #     mask[:, :, current_z] = binary_mask
 
+        mask = temperature > args.tempT
 
         # cast the ring of mask over velz and dens array, plot out the velocity profile, with integrated density as a function of velz  
-        dens_roi = np.where(mask, new_dens[:, :, args.lower_bound:args.upper_bound], np.nan)
+        dens_roi = np.where(mask, density[:, :, args.lower_bound:args.upper_bound], np.nan)
         coords = np.argwhere(~np.isnan(dens_roi))
 
         # Determine the grid size to reduce points by approximately 1/40th
-        target_size = len(coords) // 20
+        target_size = len(coords) // args.shrink_ratio
         volume = np.ptp(coords, axis=0)  # Get the range of each dimension
         grid_size = np.cbrt(np.prod(volume) / target_size)  # Calculate grid size for target reduction
 
@@ -155,12 +162,12 @@ def main(args):
         pcd.points = o3d.utility.Vector3dVector(pooled_coords)
         # Use the 'colors' attribute to store the density values as grayscale colors
         # Repeat the normalized density values across three columns to simulate grayscale (R=G=B)
-        pcd.colors = o3d.utility.Vector3dVector(np.tile(densities_normalized[:, None], (1, 3)))
+        # pcd.colors = o3d.utility.Vector3dVector(np.tile(densities_normalized[:, None], (1, 3)))
 
         o3d.io.write_point_cloud(os.path.join(args.ply_root, f"{time_Myr}.ply"), pcd)
         
         if(DEBUG):
-            print(f"Done processing time: {time_Myr}. .ply stored at: {args.ply_root}/{time_Myr}.ply")
+            print(f"Done processing time: {time_Myr}. ply file stored at: {args.ply_root}/{time_Myr}.ply")
 
 
 
@@ -182,6 +189,10 @@ if __name__ == "__main__":
     parser.add_argument("--zlim", help="Input zlim", type = int, default = 256)                                         # 256
     parser.add_argument('-pr', '--ply_root', help='Input the root path to where the k3d plots should be stored')                # '/home/joy0921/Desktop/Dataset/img_pix256/k3d_html'
     parser.add_argument("--file_prefix", help="file prefix", default="sn34_smd132_bx5_pe300_hdf5_plt_cnt_")                                                   # "sn34_smd132_bx5_pe300_hdf5_plt_cnt_"
+    parser.add_argument('-t', '--threshold', help='The threshold for slicing density (0-255 scale)', default = 120, type = int)
+    parser.add_argument('-sr', '--shrink_ratio', help='The proportion ration for the cube to be shrinked.', default = 20, type = int)
+    parser.add_argument('-tempT', '--tempT', help='The temperature threshold for the high temperature, low density region', default = 50000, type = int)
+    
     
     # parser.add_argument('-', '--', help='')
 
@@ -189,4 +200,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
 
-    # python hdf52ply.py -st 209 -et 210 --ply_root /home/joy0921/Desktop/Dataset/img_pix256/plys
+    # python hdf52ply.py -st 209 -et 210 --ply_root /home/joy0921/Desktop/Dataset/img_pix256/plys -t 110
